3fe8552b2e95fde9391f88342f1d5d992cf336bb
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-07 08:16:39 +0200

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # installing flask using pip3
 
 package { 'flask':
-ensure          => '2.1.0',
-name            => 'flask',
-provider        => 'pip3',
+ensure   => 'installed',
+name     => 'Flask==2.1.0',
+provider => 'pip3',
 }
