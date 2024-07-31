da0034246b632ab030b0e4dc788b3fd7a96f2624
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-31 23:42:07 +0300

    not important

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""get data from https://jsonplaceholder.typicode.com/"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    emp_id = argv[1]
+    emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    emp_response = requests.get(emp_url)
+    # print(response.text)
+    emp_name = emp_response.json()["name"]
+    tasks_url = f"https://jsonplaceholder.typicode.com/todos?userId={emp_id}"
+    tasks_response = requests.get(tasks_url)
+    tasks_list = tasks_response.json()
+    total_tasks_number = len(tasks_list)
+    tasks_completed_number = 0
+    tasks_completed_titles = ""
+    for task in tasks_list:
+        if task["completed"]:
+            tasks_completed_number += 1
+            tasks_completed_titles += f"\t{task['title']}\n"
+    print(f"Employee {emp_name} is done with tasks"
+          + f"({total_tasks_number}/{tasks_completed_number}):")
+    print(tasks_completed_titles, end="")
