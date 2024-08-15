839b8a4de90cac67f262b4766eba3dfa44141c0f
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-15 22:35:31 +0300

    added or modified a file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,11 @@ def number_of_subscribers(subreddit):
     """send a request to a reddit api"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64;" +
+        "rv:15.0) Gecko/20100101 Firefox/15.0.1"
+    }
+
     response = requests.get(url, allow_redirects=False)
     if response.status_code >= 400:
         return 0
