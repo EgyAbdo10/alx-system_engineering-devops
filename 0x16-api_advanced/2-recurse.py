1497f2263ffc1f9b0d2ebb3b4da06cf52b85d264
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-16 00:22:37 +0300

    added or modified a file

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""get the titles of subreddit"""
+"""get the titles of subreddit recursively"""
 
 
 import requests
 
 
-def top_ten(subreddit):
+def recurse(subreddit, hot_list=[], limit=1):
     """return the first 10 hot posts for a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by me)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {"limit": 10}
+    params = {"limit": limit}
     response = requests.get(url=url, allow_redirects=False,
                             headers=headers, params=params)
-    if response.status_code != 200:
-        print("None")
-        return
-
+    if response != 200:
+        return None
     data = response.json().get("data").get("children")
-    for item in data:
-        title = item.get("data").get("title")
-        print(title)
-    return
+    title = data[-1].get("data").get("title")
+
+    hot_list.append(title)
+    
+
