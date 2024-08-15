99862b92a12bf41f8becae63ca972a34df797814
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-15 22:23:15 +0300

    added or modified a file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,5 +17,6 @@ def number_of_subscribers(subreddit):
     data_dict = response.json()
     return data_dict["data"]["active_user_count"]
 
+
 if len(sys.argv) == 2:
-    sys.argv[1]
+    number_of_subscribers(sys.argv[1])
