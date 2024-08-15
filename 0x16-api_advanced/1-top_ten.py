aa556412270c146200cfe07ef633a601867b817d
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-15 23:20:53 +0300

    added or modified a file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,16 +7,17 @@ import requests
 
 def top_ten(subreddit):
     """return the first 10 hot posts for a subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url=url, allow_redirects=False, headers=headers)
+    params = {"limit": 10}
+    response = requests.get(url=url, allow_redirects=False,
+                            headers=headers, params=params)
     if response == 404:
         return None
 
     data = response.json().get("data").get("children")
     for item in data:
         title = item.get("data").get("title")
-        if len(title.strip()) != 0:
-            print(title)
+        print(title)
