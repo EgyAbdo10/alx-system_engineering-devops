a6ac98c39fa9ce08f743ab1f087208e133f6681e
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-15 19:46:39 +0300

    added or modified a file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """send a request to a reddit api"""
-    subreddit = argv[1]
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     response = requests.get(url, allow_redirects=False)
     if response.status_code != 200:
-        return None
+        return 0
 
     data_dict = response.json()
     return data_dict["data"]["active_user_count"]
+
+print(number_of_subscribers(argv[1]))
