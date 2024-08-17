26664716cf00e72eda8324454b4771cbffe9d833
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-17 05:49:57 +0300

    fixed the bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,33 +1,12 @@
-# fix the webstack 3
+# fix the web stack 3
 
 exec { 'create_file':
-    command => 'usr/bin/touch /var/www/html/index.html'
-}
-
-exec { 'create_file_2':
-    command => 'usr/bin/touch /var/www/html/index.cgi'
-}
-
-exec { 'create_file_3':
-    command => 'usr/bin/touch /var/www/html/index.pl'
-}
-
-exec { 'create_file_4':
-    command => 'usr/bin/touch /var/www/html/.maintenance'
-}
-
-exec { 'create_file_5':
-    command => 'usr/bin/touch /var/www/html/wp-content/languages'
-}
-
-exec { 'create_file_6':
-    command => 'usr/bin/touch /var/www/html/wp-content/db.php'
-}
-
-exec { 'create_file_7':
-    command => 'usr/bin/touch /var/www/html/wp-content/object-cache.php'
-}
-
-exec { 'create_file_8':
-    command => 'usr/bin/touch /var/www/html/wp-includes/class-wp-locale.phpp'
+    command => '/usr/bin/touch /var/www/html/index.html &&
+    /usr/bin/touch /var/www/html/index.cgi &&
+    /usr/bin/touch /var/www/html/index.pl &&
+    /usr/bin/touch /var/www/html/.maintenance &&
+    /usr/bin/touch /var/www/html/wp-content/languages &&
+    /usr/bin/touch /var/www/html/wp-content/db.php &&
+    /usr/bin/touch /var/www/html/wp-content/object-cache.php &&
+    /usr/bin/touch /var/www/html/wp-includes/class-wp-locale.phpp',
 }
