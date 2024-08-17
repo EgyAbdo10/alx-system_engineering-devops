9fb41030138254daf0408ad5c5d838e2a060c06b
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-17 07:29:13 +0300

    added or modified a file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,17 @@
 # fix the web stack 3
 
-exec { 'create_file':
-    command => '/usr/bin/touch /var/www/html/index.html &&
-    /usr/bin/touch /var/www/html/index.cgi &&
-    /usr/bin/touch /var/www/html/index.pl &&
-    /usr/bin/touch /var/www/html/.maintenance &&
-    /usr/bin/touch /var/www/html/wp-content/languages &&
-    /usr/bin/touch /var/www/html/wp-content/db.php &&
-    /usr/bin/touch /var/www/html/wp-content/object-cache.php &&
-    /usr/bin/touch /var/www/html/wp-includes/class-wp-locale.phpp &&
-    /etc/init.d/apache2 restart',
+exec {
+    command => 'sed -i 's/phpp/php/g' wp-settings.php',
 }
+
+# exec { 'create_file':
+#    command => '/usr/bin/touch /var/www/html/index.html &&
+#    /usr/bin/touch /var/www/html/index.cgi &&
+#    /usr/bin/touch /var/www/html/index.pl &&
+#    /usr/bin/touch /var/www/html/.maintenance &&
+#    /usr/bin/touch /var/www/html/wp-content/languages &&
+#    /usr/bin/touch /var/www/html/wp-content/db.php &&
+#    /usr/bin/touch /var/www/html/wp-content/object-cache.php &&
+#    /usr/bin/touch /var/www/html/wp-includes/class-wp-locale.phpp &&
+#    /etc/init.d/apache2 restart',
+# }
