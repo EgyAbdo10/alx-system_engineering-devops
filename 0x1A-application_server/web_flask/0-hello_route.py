5e851d56b8c9690deb6e72869b8611f7193becdb
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-22 23:46:38 +0300

    added or modified a file

--- 0x1A-application_server/web_flask/0-hello_route.py
+++ 0x1A-application_server/web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
-from flask import Flask
+#!/usr/bin/python3
 """create a flask web app"""
 
+from flask import Flask
+from os import environ
 
+environ['FLASK_ENV'] = 'production'
 app = Flask(__name__)
 
 
+# get /airbnb-onepage page
 @app.route("/airbnb-onepage", strict_slashes=False)
 def say_hi():
     """return Hello HBNB!"""
@@ -12,4 +16,5 @@ def say_hi():
 
 
 if __name__ == "__main__":
+    # run the application on host 0.0.0.0 and port 5000
     app.run(host="0.0.0.0", port=5000)
