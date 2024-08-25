82a2d918403119766f878364bc7161d323acc84e
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-25 07:53:44 +0300

    fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # fix the broken webstack by replacing the max number of file descriptors
 exec {'change max file descriptors':
-    command => "sed -i 's/15/2000/' /etc/default/nginx && service nginx restart"
+    command => "sed -i 's/15/4096/' /etc/default/nginx && service nginx restart"
 }
